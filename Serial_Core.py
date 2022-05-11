b32b9783aba0f6f2b8a881bb6bbac3c7eb240225
umeiko <umeko@stu.xmu.edu.cn>
2022-05-11 19:53:11 +0800

    完成

--- Serial_Core.py
+++ Serial_Core.py
@@ -1,28 +1,153 @@
+from ctypes import Union
 import threading
 import serial
+import serial.tools.list_ports
 from PySide6.QtCore import Signal, QObject
 from PySide6.QtGui import QTextCursor
+from typing import Union
+import time
 
 
-class jump_worker(QObject):
-    """一个提供信号的模块"""
-    jump_sig  = Signal(QTextCursor)
-    send_char_sig = Signal(str, bool)
-    erro_sig  = Signal()
-    def sendCursor(self, Cursor):
-        self.jump_sig.emit(Cursor)
-    def sendChar(self, char, mode):
-        self.send_char_sig.emit(char, mode)
-
-class Serial_Thread(threading.Thread):
-    """串口收发线程"""
-    pass
-
-class Serial_Maneger():
-    """串口资源管理器"""
-    def __init__(self) -> None:
-        self.Ser = serial.Serial()
+class Serial_Thread(threading.Thread, QObject):
+    """串口调试助手的收信线程"""
+    jump_sig = Signal()
+    text_sig = Signal(str)
+    def __init__(self, ser_manager):
+        threading.Thread.__init__(self)
+        QObject.__init__(self)
+        self.isRunning = False
+        self.serial_manager = ser_manager
+        self.ser = ser_manager.ser
+        self.jump_last = False
+    
+    def run(self):
+        temp = b""
+        text = b""
+        self.isRunning = True
         
-    def scan_ports()->list:
-        pass
+        
+        while self.isRunning:
+            self.serial_manager.read_lock.acquire()
+            if self.ser.isOpen():
+                try:
+                    text = self.ser.read()
+                except BaseException as e:
+                    self.serial_manager.port_erro_signal.emit("From Thread:"+str(e))
+            self.serial_manager.read_lock.release()
+
+            if text:
+                temp += text
+                text = b""
+                try:  # 解决汉字等二进制转换的问题
+                    decode_str = temp.decode(encoding="utf-8")
+                    for k, i in enumerate(decode_str):
+                        if i in "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x0e\x0f":
+                            decode_str = decode_str[:k] + "\\x" + temp[k:k+1].hex() + decode_str[k+1:]
+                    self.text_sig.emit(decode_str)
+                    temp = b""
+                
+                except BaseException as e:
+                    msg = str(e).split(":")[-1]
+                    if msg == " unexpected end of data":
+                        if len(temp) >3:
+                            decode_str = ""
+                            for k, _ in enumerate(temp):
+                                decode_str += "\\x" + temp[k:k+1].hex()
+                            self.text_sig.emit(decode_str)
+                            temp = b""
+                    else:
+                        decode_str = ""
+                        for k, _ in enumerate(temp):
+                            decode_str += "\\x" + temp[k:k+1].hex()
+                        self.text_sig.emit(decode_str)
+                        temp = b""
+                
+                if self.jump_last:
+                    self.jump_sig.emit()   
+        print("串口打印线程被终止")    
+
+
+class Serial_Manager(QObject):
+    """串口资源管理器"""
+    port_erro_signal = Signal(str)
+    port_switch_signal = Signal(bool)
+    baudrates = [300, 1200, 2400, 4800, 9600, 19200,
+                38400, 57600, 74880, 115200, 230400, 
+                250000, 500000, 1000000, 2000000,]
+    formats = [ (5, "N", 1), (5, "N", 2), (6, "N", 1), (6, "N", 1),
+                (7, "N", 1), (7, "N", 2), (8, "N", 1), (8, "N", 2),
+                (5, "E", 1), (5, "E", 2), (6, "E", 1), (6, "E", 1),
+                (7, "E", 1), (7, "E", 2), (8, "E", 1), (8, "E", 2),
+                (5, "O", 1), (5, "O", 2), (6, "O", 1), (6, "O", 1),
+                (7, "O", 1), (7, "O", 2), (8, "O", 1), (8, "O", 2),
+                ]
+    def __init__(self) -> None:
+        super().__init__()
+        self.ser = serial.Serial()
+        self.ser.baudrate = 115200
+        self.ser.timeout = 0.005
+        self.read_lock = threading.Lock()
+        self.write_lock = threading.Lock() 
+        self.end_char = b""
+        
+    
+    def set_format(self, bytesize, parity, stopbits):
+        """设置串口格式：数据位，校验，停止位"""
+        print(bytesize, parity, stopbits)
+        self.ser.bytesize, self.ser.parity, self.ser.stopbits = bytesize, parity, stopbits
+
+    def set_baudrate(self, baudrate):
+        print(baudrate)
+        self.ser.baudrate = baudrate
+    
+    def scan_ports(self)->tuple:
+        """查询有哪些串口可用, 返回两个列表"""
+        options = serial.tools.list_ports.comports()
+        ports = [i.device for i in options]
+        names = [i.description for i in options]
+        return ports, names
+
+    def open_port(self, port:str):
+        if (self.ser.isOpen() and port != self.ser.port):
+            self.read_lock.acquire()
+            self.write_lock.acquire()            
+            self.ser.close()
+            self.read_lock.release()
+            self.write_lock.release() 
+        self.ser.port = port
+        if not self.ser.isOpen():
+            try:
+                self.ser.open()
+                self.port_switch_signal.emit(True)
+            except BaseException as e:
+                self.port_erro_signal.emit(str(e))
+
+    def close_port(self):
+        """关闭串口"""
+        self.read_lock.acquire()
+        self.write_lock.acquire()
+        if self.ser.isOpen():
+            self.ser.close()
+            self.port_switch_signal.emit(False)
+        self.read_lock.release()
+        self.write_lock.release()
+    
+    def write_ser(self, content:Union[str, bytes])->bool:
+        """直接写内容到串口"""
+        if isinstance(content, bytes):
+            content = content
+        elif isinstance(content, str):
+            content = content.encode()
+        else:
+            return False
+        if self.ser.isOpen():
+            self.write_lock.acquire()
+            try:
+                self.ser.write(content+self.end_char)
+            except BaseException as e:
+                self.port_erro_signal.emit(str(e))
+            self.write_lock.release()
+            return True
+        else:
+            return False
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from concurrent.futures import thread
 from mainWindow import Ui_MainWindow
 import Serial_Core
+from PySide6.QtGui import QIcon, QShortcut
 from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
 import sys
 
@@ -7,30 +9,111 @@ main_window = Ui_MainWindow() # 主界面
 app = QApplication(sys.argv)
 w = QMainWindow()
 main_window.setupUi(w)
-serial_maneger = Serial_Core.Serial_Maneger()
+main_window.baudrates = [
+    main_window.action300,
+    main_window.action1200,
+    main_window.action2400,
+    main_window.action4800,
+    main_window.action9600,
+    main_window.action19200,
+    main_window.action38400,
+    main_window.action57600,
+    main_window.action74880,
+    main_window.action115200,
+    main_window.action230400,
+    main_window.action250000,
+    main_window.action500000,
+    main_window.action1000000,
+    main_window.action2000000,
+]
+main_window.formats = [
+    main_window.action5N1,
+    main_window.action5N2,
+    main_window.action6N1,
+    main_window.action6N2,
+    main_window.action7N1,
+    main_window.action7N2,
+    main_window.action8N1,
+    main_window.action8N2,
+    main_window.action5E1,
+    main_window.action5E2,
+    main_window.action6E1,
+    main_window.action6E2,
+    main_window.action7E1,
+    main_window.action7E2,
+    main_window.action8E1,
+    main_window.action8E2,
+    main_window.action5O1,
+    main_window.action5O2,
+    main_window.action6O1,
+    main_window.action6O2,
+    main_window.action7O1,
+    main_window.action7O2,
+    main_window.action8O1,
+    main_window.action8O2,
+]
+serial_manager = Serial_Core.Serial_Manager()
+serial_thread = Serial_Core.Serial_Thread(serial_manager)
+Cursor = main_window.recv_Text.textCursor()
 
 global_options = {
     "temp_ports_list": [],
     "last_port"      : 0,
     "end_char"       : 0,
-    "skin_mode"      : "classic",
+    "auto_last"      : False,
+    "baudrate"       : 9,
+    "format"         : 6,
+    "skin_mode"      : "Classic",
 }
 
 def init_methods():
-    pass
+    load_options()
+    main_window.statusbar.showMessage("Designed By Umeko")
+    serial_thread.start()
 
 def close_methods(*args):
-    pass
+    save_options()
+    serial_thread.isRunning = False
+    serial_manager.close_port()
 
 def bind_methods():
-    pass
+    """为组件绑定功能"""
+    serial_manager.port_erro_signal.connect(func_for_serial_erro)
+    main_window.port_select.mousePressEvent = func_for_show_ports
+    main_window.port_select.currentIndexChanged.connect(func_for_select_port) 
+    main_window.port_select.wheelEvent=lambda *args: None
+    main_window.end_select.currentIndexChanged.connect(func_for_select_end_char)
+    
+    shortcut = QShortcut(w)
+    shortcut.setKey(u'Return')
+    shortcut.activated.connect(func_for_send_serial_msg)
+    main_window.Sending.clicked.connect(func_for_send_serial_msg)
+    main_window.Clear.clicked.connect(main_window.recv_Text.clear)
+    main_window.AutoLast.toggled.connect(func_jump_to_last_line)
+
+    main_window.menu_2.triggered.connect(func_for_change_baudrate)
+    main_window.menu_3.triggered.connect(func_for_change_format)
+    main_window.Default.triggered.connect(set_defaut)
+
+    main_window.style_dark.triggered.connect(lambda: change_style("MaterialDark"))
+    main_window.style_classic.triggered.connect(lambda: change_style("Classic"))
+    
+    serial_thread.text_sig.connect(Cursor.insertText)
+    serial_thread.jump_sig.connect(func_jump_to_last_line)
+
+
+
+def func_for_show_ports(*args):
+    """展示串口的函数"""
+    fresh_ports()
+    print(global_options)
+    main_window.port_select.showPopup()
 
 def fresh_ports():
-    global main_window
     """刷新系统当前连接的串口设备"""
-    
+    global main_window
     main_window.port_select.setItemText(0, "断开连接")
-    ports, names = serial_maneger.scan_ports()
+    ports, names = serial_manager.scan_ports()
 
     for k, i in enumerate(global_options["temp_ports_list"]):
         # 删除已不存在的端口
@@ -45,12 +128,111 @@ def fresh_ports():
             main_window.port_select.addItem(i)
             if global_options["temp_ports_list"]:
                 global_options["temp_ports_list"].append(ports[k])
-    
     if not global_options["temp_ports_list"]:
         global_options["temp_ports_list"] = ports
 
+def func_for_select_port(*args):
+    """选择连接到某个串口"""
+    index = args[0]
+    if index > 0:
+        serial_manager.open_port(global_options["temp_ports_list"][index-1])
+    else:
+        serial_manager.close_port()
+    global_options["last_port"] = index
+    print(index)
+
+def func_for_select_end_char(*args):
+    '''串口监视器选择结束符的函数'''
+    ends = [b"", b"\n", b"\r", b"\r\n"]
+    global_options["end_char"]=args[0]
+    serial_manager.end_char = ends[args[0]]
+
+def func_for_send_serial_msg(*args):
+    """发送串口信息的函数"""
+    msg = main_window.sendingTextEdit.text()
+    serial_manager.write_ser(msg)
+    main_window.sendingTextEdit.clear()
+
+def func_jump_to_last_line(*args):
+    """跳到最后一行的函数"""
+    if args:
+        serial_thread.jump_last = args[0]
+        global_options["auto_last"] = args[0]
+    main_window.recv_Text.setTextCursor(Cursor)
+
+def func_for_serial_erro(*args):
+    """串口异常处理的函数"""
+    print(args)
+    serial_manager.close_port()
+    main_window.port_select.setItemText(0, "连接失败, 请重试")
+    main_window.port_select.setCurrentIndex(0)
+
+def load_options():
+    global global_options
+    import json as json
+    try:
+        with open("main_config.json", 'r') as js_file:
+            temp_robo_options = json.load(js_file)
+    
+    except BaseException as e:
+        with open("main_config.json", 'w') as js_file:
+            js_string = json.dumps(global_options, sort_keys=True, indent=4, separators=(',', ': '))
+            js_file.write(js_string)
+        temp_robo_options = global_options.copy()
+    
+    fresh_ports()
+    for key in global_options:
+        try:
+            temp_robo_options[key]
+        except Exception:
+            temp_robo_options[key] = global_options[key]
+    
+    if temp_robo_options["temp_ports_list"] == global_options["temp_ports_list"]:
+        main_window.port_select.setCurrentIndex(temp_robo_options["last_port"])
+
+    main_window.end_select.setCurrentIndex(temp_robo_options["end_char"])
+    main_window.AutoLast.setChecked(temp_robo_options["auto_last"])
+    serial_thread.jump_last = temp_robo_options["auto_last"]
+    change_style(temp_robo_options["skin_mode"])
+    func_for_change_baudrate(main_window.baudrates[temp_robo_options["baudrate"]])
+    func_for_change_format(main_window.formats[temp_robo_options["format"]])
+
+def change_style(style:str):
+    """设置皮肤"""
+    import qss
+    if global_options["skin_mode"] != style:
+        global_options["skin_mode"] = style
+        w.setStyleSheet(qss.styles[style])
+
+def save_options():
+    """导出主窗口的配置到文件中"""
+    import json as json
+    with open("main_config.json", 'w') as js_file:
+        js_string = json.dumps(global_options, sort_keys=True, indent=4, separators=(',', ': '))
+        js_file.write(js_string)
+
+def func_for_change_baudrate(Qaction):
+    [i.setChecked(False) for i in main_window.baudrates]
+    Qaction.setChecked(True)
+    serial_manager.set_baudrate(int(Qaction.text()))
+    global_options["baudrate"] = main_window.baudrates.index(Qaction)
+    
+
+def func_for_change_format(Qaction):
+    [i.setChecked(False) for i in main_window.formats]
+    Qaction.setChecked(True)
+    b, p, s = Qaction.text()[0], Qaction.text()[1], Qaction.text()[2]
+    serial_manager.set_format(int(b), p, int(s))
+    global_options["format"] = main_window.formats.index(Qaction)
+
+def set_defaut():
+    """恢复初始通讯设置"""
+    func_for_change_baudrate(main_window.action115200)
+    func_for_change_format(main_window.action8N1)
 
 def main():
+    bind_methods()
+    init_methods()
     w.closeEvent = close_methods
     w.show()
     sys.exit(app.exec())
